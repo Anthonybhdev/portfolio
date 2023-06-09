7182dbca22e4d667777583f4a91d09f09fb03f7e
Anthonybhdev <124305757+Anthonybhdev@users.noreply.github.com>
2023-06-09 15:23:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ ScrollReveal().reveal('.home-content p , .about-content', { origin: 'right'});
 
  /* ==================================== typed js ========================================*/
 const typed = new Typed(".multiple-text", {
-    strings: ['Frontend Developer', "HTML Email Developer"],
+    strings: ['Frontend Developer', "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
